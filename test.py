bf26c4c1cb4fe92baaaff79b1f8740a4a05f100d
saks004 <117853096+saks004@users.noreply.github.com>
2022-12-11 00:11:20 -0500

    added a sum function

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-import pandas as pd
+
+#comment
+#adding two strings
+
+def sum (a,b):
+    add = a+b
+    print (add)
+    return(add)
+
+def view (num):
+    for x in range(num):
+        sum(x,x+1)
+view(10)
