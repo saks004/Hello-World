51e65b3799261e8f4cc3bc84ddc83e7cc7902c80
saks004 <117853096+saks004@users.noreply.github.com>
2023-01-03 13:21:32 -0500

    csv file and code

--- practice.py
+++ practice.py
@@ -1,54 +1,28 @@
 import os
 import pprint
+import csv
+
 filemap= []
 file_name= []
 def read_directory(path):
     
     dir_list= os.listdir(path)
     for file in dir_list:
-        if file.endswith(".txt"): 
+        if file.endswith(".csv"): 
             file_name.append(file)
             a= process_file(file)          
             filemap.append(a)
        
     return(filemap)                                     
-     
-#Total count of lines in a txt file
+
+line=[]
 def process_file(filename):
-    
-    file_dictionary ={
-        "file_name": [],
-        "total_lines" : 0,
-        "line_index" : [],
-        "line_list" : [],
-        "total_words" : 0
-    }
-    for name in file_name:
-
-        with open(filename, 'r') as fp:
-            line_list, line_indexes, total_words = [],[],0
-            for count, line in enumerate(fp):
-                line_list.append(line.strip())
-                line_indexes.append(count)
-                total_words= total_words + len(line.split())
-
-            # print("Lines in the file", count, line)
-
-    file_dictionary = {
-        "file_name": name,
-        "total_lines" : count+1,
-        "line_index" : line_indexes,
-        "line_list" : line_list,
-        "total_words": total_words 
-    }   
-    return (file_dictionary)
+    with open(filename, 'r') as fp:
+            cereals= csv.reader(fp)
+            for values in cereals:
+                line.append(values)
+    return(line)
 
 b= read_directory("//Users//sakshumsingh//Workspace//Python Practice")
 
 pprint.pprint(b)
-
-
-
-
-
-
