2004afdaf6018de60da1de58e9b0581a06302541
saks004 <117853096+saks004@users.noreply.github.com>
2023-01-05 22:53:15 -0500

    Return data frame for a CSV file

--- practice.py
+++ practice.py
@@ -1,28 +1,34 @@
 import os
 import pprint
 import csv
+import pandas as pd
 
-filemap= []
-file_name= []
+
 def read_directory(path):
-    
+    new_df= pd.DataFrame() 
+    file_name= []      
     dir_list= os.listdir(path)
     for file in dir_list:
         if file.endswith(".csv"): 
             file_name.append(file)
-            a= process_file(file)          
-            filemap.append(a)
-       
-    return(filemap)                                     
+            new_df.append(process_file(file))          
+   
+    return(new_df)                                     
 
-line=[]
+
 def process_file(filename):
-    with open(filename, 'r') as fp:
-            cereals= csv.reader(fp)
-            for values in cereals:
-                line.append(values)
-    return(line)
+    
+        df= pd.read_csv(filename)
+        return(df)
+   
 
 b= read_directory("//Users//sakshumsingh//Workspace//Python Practice")
 
 pprint.pprint(b)
+
+
+# def func(name):
+
+#     df= pd.read_csv()
+#     print (df.head())
+#     return df
